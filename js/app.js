517967efb90e3ba98401f8aae256bd5d83bf7a9d
Javier Enrique <s0wkete@gmail.com>
2015-01-09 13:00:44 -0300

    Se termina programacion

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+
+window.onload = function() {
+    $('#program .inline-list .header').css("height", $('#program .inline-list').height());
+};
+$( window ).resize(function() {
+    $('#program .inline-list .header').css("height", $('#program .inline-list').height());
+});
+
