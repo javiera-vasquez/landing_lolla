a1fdfa6072e03cad0cfed1801300af44bccc27db
Javier Enrique <s0wkete@gmail.com>
2015-01-12 12:38:22 -0300

    Revised for responsive

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 $(document).foundation();
 
+// Know the width of the viewport
+var windowWidth = $(window).width();
+
 window.onload = function() {
-    $('#program .inline-list .header').css("height", $('#program .inline-list').height());
+    if(windowWidth >= 650) {
+        $('#program .inline-list .header').css("height", $('#program .inline-list').height());
+    };
 };
+
 $( window ).resize(function() {
-    $('#program .inline-list .header').css("height", $('#program .inline-list').height());
+    $('#program .inline-list .header').css("height", $('#program .inline-list li:nth-child(2)').height());
 });
+
+
+
+
+
 
